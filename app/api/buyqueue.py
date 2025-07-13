62d9f23f3fe3d0ba6db2c5ca5ea5ef86b4f0d019
dizon69 <dizonfirst@gmail.com>
2025-07-13 21:15:39 +0700

    update

--- app/api/buyqueue.py
+++ app/api/buyqueue.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from app.services.binance_ws import get_top_buyqueue
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 @router.get("/buyqueue")
 def get_buyqueue():
-    return get_top_buyqueue()
+    data = get_top_buyqueue()
+    return JSONResponse(content=data)
