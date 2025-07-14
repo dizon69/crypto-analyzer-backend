4d470d6122b4a557fcbca491a80caa0e6ac97df0
dizon69 <dizonfirst@gmail.com>
2025-07-14 22:04:55 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-### main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.buyqueue import router as buyqueue_router
 
 app = FastAPI()
+
+# ⬇️ Tambahkan middleware CORS di sini
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://crypto-analyzer.com,"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(buyqueue_router, prefix="/api", tags=["BuyQueue"])
