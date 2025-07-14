be3adb11b68916c745e3aaaa93e5d6d39fe095bc
dizon69 <dizonfirst@gmail.com>
2025-07-14 22:48:28 +0700

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://crypto-analyzer.com",
+                   "https://www.crypto-analyzer.com",
                    "https://crypto-analyzer-frontend.vercel.app",
                    "http://localhost:5173"],
     allow_credentials=True,
