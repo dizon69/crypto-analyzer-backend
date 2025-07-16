0733b0a62bfa2a693ecd06df255eae39abebc166
dizon69 <dizonfirst@gmail.com>
2025-07-17 00:01:01 +0700

    update

--- routes/debug.js
+++ routes/debug.js
@@ -2,7 +2,7 @@
 const { dequeMap } = require("../collector/binance_ws");
 
 async function routes(fastify, opts) {
-  fastify.get("/debug/deque", async (req, reply) => {
+  fastify.get("/__debug/deque", async (req, reply) => {
     const result = {};
     for (const [symbol, deque] of dequeMap.entries()) {
       result[symbol] = deque.map(x => +x.ratio.toFixed(2));
