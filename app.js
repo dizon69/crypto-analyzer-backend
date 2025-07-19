4f16a2c79f0d2f415a334dd5dedd6448e5ad9b51
dizon69 <dizonfirst@gmail.com>
2025-07-19 14:55:17 +0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cors = require("@fastify/cors");
 
 const webhookRoute = require("./webhook");
 const buyqueueRoutes = require("./routes/buyqueue");
-const debugRoutes = require("./routes/debug");
 const depthRoutes = require("./routes/depth"); // ✅ Tambahkan ini
 require("./collector/binance_ws_futures");
 require("./collector/depth_ws"); // ✅ Jangan lupa koneksikan WS-nya
