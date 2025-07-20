00e68434c1bbd7033e88f0e06dab0274fe602084
dizon69 <dizonfirst@gmail.com>
2025-07-20 19:09:23 +0700

    update

--- config/symbols.js
+++ config/symbols.js
@@ -3,5 +3,11 @@ module.exports = [
   "dogeusdt", "xrpusdt", "linkusdt", "solusdt", "maticusdt",
   "avaxusdt", "dotusdt", "trxusdt", "bchusdt", "uniusdt",
   "atomusdt", "etcusdt", "opusdt", "nearusdt", "xlmusdt",
-  "enausdt", "taousdt", "seiusdt", "1000satsusdt", "fetusdt"
+  "enausdt", "taousdt", "seiusdt", "1000satsusdt", "fetusdt",
+  "arbusdt", "sandusdt", "filusdt", "egldusdt", "runeusdt",
+"injusdt", "ldousdt", "aaveusdt", "snxusdt", "galausdt", 
+"imxusdt", "chzusdt", "enjusdt", "sushiusdt", "manausdt",
+"ftmusdt", "cotiusdt", "qntusdt", "oasusdt", "zkusdt",
+"fxsusdt", "rdntusdt", "roseusdt", "cakeusdt", "maskusdt"
+
 ];
